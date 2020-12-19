71ec97e61ba062ba1771d37d2e285f300e3f0af5
AndrewPA04 <62768543+AndrewPA04@users.noreply.github.com>
2020-12-19 19:25:53 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,12 +5,12 @@ import bs4
 
 app = Flask(__name__)
 
-@app.route('https://cov-counter.herokuapp.com/')
+@app.route('/')
 def indexPage():
 
     return render_template("index.html") 
 
-@app.route('https://cov-counter.herokuapp.com/api/recordstats',methods=['GET'])
+@app.route('/api/recordstats',methods=['GET'])
 def sendJSON():
     response = requests.get("https://www.worldometers.info/coronavirus/")
     content = response.content
