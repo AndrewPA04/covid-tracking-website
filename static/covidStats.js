602cbe1aa9df6c5b4645d730f93868af5077aaf0
AndrewPA04 <62768543+AndrewPA04@users.noreply.github.com>
2020-12-19 15:27:14 +0200

    Update covidStats.js

--- static/covidStats.js
+++ static/covidStats.js
@@ -1,6 +1,10 @@
 async function getJSON(){
     const statsTable = await fetch("http://127.0.0.1:5000/api/recordstats");
     const stats = await statsTable.json();
-    console.log(stats);
-    var totalCases = stats.casesJSON + stats.recoveriesJSON + stats.deathsJSON;
+    var deathsLabel = document.getElementById('deaths');
+    deathsLabel.innerHTML = stats.deathsJSON;
+    var recoveriesLabel = document.getElementById('recoveries');
+    recoveriesLabel.innerHTML = stats.recoveriesJSON;
+    var activeLabel = document.getElementById('active-cases');
+    activeLabel.innerHTML = stats.casesJSON;
 }
