28f81d26c3342669813aac2e32edaa4d79c2ff2d
AndrewPA04 <62768543+AndrewPA04@users.noreply.github.com>
2020-12-19 12:44:27 +0200

    Delete covidStats.js

--- covidStats.js
+++
@@ -1,6 +0,0 @@
-async function getJSON(){
-    const statsTable = await fetch("http://127.0.0.1:5000/api/recordstats");
-    const stats = await statsTable.json();
-    console.log(stats);
-    var totalCases = stats.casesJSON + stats.recoveriesJSON + stats.deathsJSON;
-}
